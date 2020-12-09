b69068f9aa6c47c5683a743de0e9d043837cff7e
ViskaWei <swei20@jhu.edu>
2020-12-09 01:42:08 -0500

    add h5py

--- src/dataset/save.py
+++ src/dataset/save.py
@@ -2,23 +2,12 @@ import h5py
 import numpy as np
 
 
-def save_dataset(out,logFlux,wave):
-    with h5py.File(f'{o}.hdf5', 'w') as f:
-    #     f.create_dataset('normFlux', shape=(14, 15,16094),data=normFlux)
-        f.create_dataset('logFlux', shape=logFlux.shape, data=logFlux)  
+def save_dataset(fileName,data,dataName):
+    with h5py.File(f'{fileName}.hdf5', 'w') as f:
+        f.create_dataset(dataName, shape=data.shape, data=data)  
 
-
-def load_logFlux(DATASET):
-    with h5py.File(DATASET, 'r') as f: 
-        logFlux = f['logFlux'][()]
-        wave = f['wave'][()]
-    print(logFlux.shape)
-    return logFlux, wave
-
-def load_dataset(DATASET, sl=None):
-    if sl is None: sl = np.s_[:, 6:21, 8, 3, 1,:]
-    with h5py.File(DATASET, 'r') as f: 
-        flux = f['flux'][sl]
-        wave = f['wave'][sl[-1]]
-    print(flux.shape)
-    return flux, wave
+def load_dataset(fileName, dataName, sl=None):
+    with h5py.File(f'{fileName}.hdf5', 'r') as f: 
+        data = f[dataName][sl]
+    print(f"loading {dataName} of size {data.shape}")
+    return data
